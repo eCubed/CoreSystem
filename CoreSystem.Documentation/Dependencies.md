98b742904d41e74d5ed7790c4abf4ff9bc8af5d1
Israel D. Fernando <mr.i.d.fernando@gmail.com>
2017-09-26 22:45:31 -0500

    Adds File Server (Secured)

--- CoreSystem.Documentation/Dependencies.md
+++ CoreSystem.Documentation/Dependencies.md
@@ -18,5 +18,14 @@
 
 ## CoreSystem.ResourceServer
 * Microsoft.AspNetCore (because it pre-installed 1.1.2, so we installed 2.0.0 instead)
-* 
+* Microsoft.AspNetCore.Cors
+* Microsoft.AspNetCore.Mvc
+* Newtonsoft.Json
+
+## CoreSystem.FileServer
+* Microsoft.AspNetCore (because it pre-installed 1.1.2, so we installed 2.0.0 instead)
+* Microsoft.AspNetCore.Cors
+* Microsoft.AspNetCore.Mvc
+* Newtonsoft.Json
+
 
---
+++ CoreSystem.FileServer/Controllers/UploadController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreSystem.FileServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class UploadController : Controller
+    {
+        private IHostingEnvironment hostingEnvironment { get; set; }
+
+        public UploadController(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
+        // POST api/values
+        [Authorize(Roles = "user,admin")]
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            foreach(var file in Request.Form.Files)
+            {
+                string filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+
+                filename = EnsureCorrectFilename(filename);
+
+                using (FileStream output = System.IO.File.Create(GetPathAndFilename(filename)))
+                {
+                    file.CopyTo(output);
+                    output.Flush();
+                }
+            }
+
+            return await Task.FromResult(StatusCode(201));
+        }
+
+        private string EnsureCorrectFilename(string filename)
+        {
+            if (filename.Contains("\\"))
+                filename = filename.Substring(filename.LastIndexOf("\\") + 1);
+
+            return filename;
+        }
+
+        private string GetPathAndFilename(string filename)
+        {
+            DateTime now = DateTime.Now;
+
+            string dirPath = $"{hostingEnvironment.WebRootPath}\\{now.Year}\\{now.Month}";
+            if (!Directory.Exists(dirPath)) {
+                Directory.CreateDirectory(dirPath);
+            }
+
+            return $"{dirPath}\\{now.ToString("yyyyMMddHHmmssffff")}_{filename}";
+        }
+
+    }
+}
