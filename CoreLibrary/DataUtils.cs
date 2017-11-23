8cc866e4ddb861620d27da7a336e94d6e6198cd9
Unknown <mr.i.d.fernando@gmail.com>
2017-11-23 13:54:19 -0600

    Permission added for Create, Update, Delete
    As a last test to go ahead with the create, update, and delete, we included optional function callback.

--- CoreLibrary/DataUtils.cs
+++ CoreLibrary/DataUtils.cs
@@ -6,7 +6,7 @@ namespace CoreLibrary
     public static class DataUtils
     {
         public static async Task<ManagerResult> CreateAsync<T, TKey>(T entity, IAsyncStore<T, TKey> store,
-            Func<T, Task<T>> findUniqueAsync = null)
+            Func<T, Task<T>> findUniqueAsync = null, Func<T, ManagerResult> canCreate = null)
             where T : class, IIdentifiable<TKey>
         {
             if (findUniqueAsync != null)
@@ -17,12 +17,21 @@ namespace CoreLibrary
                     return new ManagerResult(ManagerErrors.DuplicateOnCreate);
             }
 
+            if (canCreate != null)
+            {
+                var createRes = canCreate.Invoke(entity);
+
+                if (!createRes.Success)
+                    return createRes;
+            }
+
             await store.CreateAsync(entity);           
 
             return new ManagerResult();
         }
 
-        public static async Task<ManagerResult> DeleteAsync<T, TKey>(TKey id, IAsyncStore<T, TKey> store)
+        public static async Task<ManagerResult> DeleteAsync<T, TKey>(TKey id, IAsyncStore<T, TKey> store,
+            Func<T, ManagerResult> canDelete = null)
              where T : class, IIdentifiable<TKey>
         {
             try
@@ -32,6 +41,13 @@ namespace CoreLibrary
                 if (found == null)
                     return new ManagerResult(ManagerErrors.RecordNotFound);
                 
+                if (canDelete != null)
+                {
+                    var canDeleteRes = canDelete.Invoke(found);
+
+                    if (!canDeleteRes.Success)
+                        return canDeleteRes;
+                }
 
                 await store.DeleteAsync(found);
             }
@@ -43,22 +59,35 @@ namespace CoreLibrary
             return new ManagerResult();
         }
 
-        public static async Task<ManagerResult> UpdateAsync<T, TKey>(T entity, IAsyncStore<T, TKey> store,
-            Func<T, Task<T>> findUniqueAsync = null)
+        public static async Task<ManagerResult> UpdateAsync<T, TKey>(TKey id, IAsyncStore<T, TKey> store,
+            Func<T, Task<T>> findUniqueAsync = null, Func<T, ManagerResult> canUpdate = null,
+            Action<T> fillNewValues = null)
             where T : class, IIdentifiable<TKey>
         {
-            T recordToUpdate = await store.FindByIdAsync(entity.Id);
+            T recordToUpdate = await store.FindByIdAsync(id);
 
             if (recordToUpdate == null)
                 return new ManagerResult(ManagerErrors.RecordNotFound);
 
             if (findUniqueAsync != null)
             {
-                T possibleDuplicate = await findUniqueAsync(entity);
+                T possibleDuplicate = await findUniqueAsync(recordToUpdate);
 
                 if ((possibleDuplicate != null) && (!possibleDuplicate.Id.Equals(recordToUpdate.Id)))
                     return new ManagerResult(ManagerErrors.DuplicateOnUpdate);
-            }                
+            }
+
+            if (canUpdate != null)
+            {
+                var canUpdateRes = canUpdate.Invoke(recordToUpdate);
+
+                if (!canUpdateRes.Success)
+                    return canUpdateRes;
+            }
+
+            // Now, we allow updating of the values!
+            if (fillNewValues != null)
+                fillNewValues.Invoke(recordToUpdate);
                 
             await store.UpdateAsync(recordToUpdate);
            
--- CoreSystem/ContactManager.cs
+++ CoreSystem/ContactManager.cs
@@ -17,17 +17,12 @@ namespace CoreSystem
             return (IContactStore<TContact>)Store;
         }
 
-        public async override Task<TContact> FindUniqueAsync(TContact matchAgainst)
+        public async Task<TContact> FindUniqueAsync(TContact matchAgainst)
         {
             return await GetContactStore().FindContactAsync(matchAgainst.FirstName, matchAgainst.LastName, matchAgainst.UserId);
         }
 
         #region Create
-
-        public override Task<ManagerResult> CreateAsync(TContact entity)
-        {
-            throw new NotSupportedException();
-        }
         
         public virtual async Task<ManagerResult> CreateAsync(SaveContactViewModel scvm, int userId)
         {
@@ -36,7 +31,7 @@ namespace CoreSystem
             newContact.UserId = userId;
 
             // To do: save the id of the contact back to the scvm!
-            var res = await base.CreateAsync(newContact);
+            var res = await DataUtils.CreateAsync(newContact, GetContactStore(), FindUniqueAsync);
 
             if (!res.Success)
                 return res;
@@ -44,70 +39,36 @@ namespace CoreSystem
             scvm.Id = newContact.Id;
 
             return new ManagerResult();
-
         }
 
         #endregion
 
         #region Update
-
-        public override Task<ManagerResult> UpdateAsync(TContact entity)
-        {
-            throw new NotSupportedException();
-        }
-
+        
         public virtual async Task<ManagerResult> UpdateAsync(SaveContactViewModel scvm, int userId)
         {
-            if (!scvm.Id.HasValue)
-                return new ManagerResult(ManagerErrors.IdNotSpecified);
+            return await DataUtils.UpdateAsync(scvm.Id.Value, GetContactStore(), FindUniqueAsync, contact =>
+            {
+                if (contact.UserId != userId)
+                    return new ManagerResult(ManagerErrors.Unauthorized);
 
-            TContact contact = await FindByIdAsync(scvm.Id.Value);
-
-            if (contact == null)
-                return new ManagerResult(ManagerErrors.RecordNotFound);
-
-            if (contact.UserId != userId)
-                return new ManagerResult(ManagerErrors.Unauthorized);
-
-            scvm.UpdateValues(contact);
-
-            return await base.UpdateAsync(contact);
-        }
-
-        public override void OnUpdatePropertyValues(TContact original, TContact entityWithNewValues)
-        {
-            original.Address1 = entityWithNewValues.Address1;
-            original.Address2 = entityWithNewValues.Address2;
-            original.City = entityWithNewValues.City;
-            original.Country = entityWithNewValues.Country;
-            original.Email = entityWithNewValues.Email;
-            original.FirstName = entityWithNewValues.FirstName;
-            original.LastName = entityWithNewValues.LastName;
-            original.Phone = entityWithNewValues.Phone;
-            original.PostalCode = entityWithNewValues.PostalCode;
-            original.Region = entityWithNewValues.Region;
+                return new ManagerResult();
+            }, contact => scvm.UpdateValues(contact));
         }
 
         #endregion
 
         #region Delete
-
-        public override Task<ManagerResult> DeleteAsync(int id)
-        {
-            throw new NotSupportedException();
-        }
-
+        
         public async Task<ManagerResult> DeleteAsync(int id, int userId)
         {
-            TContact contact = await FindByIdAsync(id);
+            return await DataUtils.DeleteAsync(id, GetContactStore(), c =>
+            {
+                if (c.UserId != userId)
+                    return new ManagerResult(ManagerErrors.Unauthorized);
 
-            if (contact == null)
-                return new ManagerResult(ManagerErrors.RecordNotFound);
-
-            if (contact.UserId != userId)
-                return new ManagerResult(ManagerErrors.Unauthorized);
-
-            return await base.DeleteAsync(id);
+                return new ManagerResult();
+            }); // base.DeleteAsync(id);
         }
 
         #endregion
