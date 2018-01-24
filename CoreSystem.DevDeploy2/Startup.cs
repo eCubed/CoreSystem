e6d29e9ab446e6b7b4b0ee7b7b156ef4dea06194
Unknown <mr.i.d.fernando@gmail.com>
2018-01-24 09:44:25 -0600

    DevDeploy ala 2.0 working

---
+++ CoreSystem.DevDeploy2/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using CoreSystem.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace CoreSystem.DevDeploy2
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = Configuration["ConnectionString"];
+            services.AddDbContext<CoreSystemDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString, opts => {
+                    //opts.MigrationsAssembly("CoreSystem.DevDeploy");
+                    opts.UseRowNumberForPaging();
+                });
+            });
+
+            services.AddIdentity<CoreSystemUser, CoreSystemRole>()
+                .AddEntityFrameworkStores<CoreSystemDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddTransient<Seeder>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, Seeder seeder)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            seeder.InitializeDataAsync().Wait();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
